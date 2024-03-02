ccad946ce87b4564b42b8fecedbad23ec1003113
KIm Seo Young <jmh7367@naver.com>
2024-03-02 11:00:04 +0000

    Design: Header 골격 완성

--- memo/src/components/Header.jsx
+++ memo/src/components/Header.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function Header() {
   return (
     <>
-      <header>
+      <HeaderStyle>
         <h1>메모장</h1>
-      </header>
+      </HeaderStyle>
     </>
   );
 }
+
+const HeaderStyle = styled.header`
+  background-color: var(--color-black);
+  grid-area: header;
+
+  h1 {
+    font-size: 45px;
+    color: var(--color-white);
+    text-align: center;
+    line-height: 75px;
+    height: 80px;
+  }
+`;
