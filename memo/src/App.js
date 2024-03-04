20511c7fe0b7ea9f3e540606db201b0276af52da
KIm Seo Young <jmh7367@naver.com>
2024-03-04 16:36:26 +0900

    Dir: NavBar 파일 생성 및 App.js에 import

--- memo/src/App.js
+++ memo/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import GlobalStyles from "./style/GlobalStyle";
 import Header from "./components/Header";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <GlobalStyles />
       <Header />
+      <NavBar />
     </>
   );
 }
