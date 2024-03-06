9bb946bb9b547521edb789e17fdff567e5232dd1
KIm Seo Young <jmh7367@naver.com>
2024-03-06 16:35:20 +0900

    Design: header, navbar 배치

--- memo/src/App.js
+++ memo/src/App.js
@@ -2,15 +2,24 @@ import React from "react";
 import GlobalStyles from "./style/GlobalStyle";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
+import styled from "styled-components";
 
 function App() {
   return (
-    <>
+    <Container>
       <GlobalStyles />
       <Header />
       <NavBar />
-    </>
+    </Container>
   );
 }
 export default App;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-areas:
+    "header header"
+    "navbar main";
+  grid-template-columns: 240px auto;
+`;
 
