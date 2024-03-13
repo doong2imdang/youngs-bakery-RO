e6b8c66260b50196b428950cfbe9b08461fe4e94
KIm Seo Young <jmh7367@naver.com>
2024-03-13 16:23:21 +0900

    Feat: 날씨 page 기능 완성

--- memo/src/pages/Weather.jsx
+++ memo/src/pages/Weather.jsx
@@ -1,20 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import Sun from "../assets/images/01d.png";
 
 export default function Weather() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [iconURL, setIconURL] = useState(null);
+
+  const API_KEY = "7a6a90c96c3f2c1fd8fcfc41c610e8ff";
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success, fail);
+  }, []);
+
+  const success = (position) => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    getWeather(latitude, longitude);
+  };
+
+  const fail = () => {
+    alert("좌표를 받아올 수 없음");
+  };
+
+  const getWeather = (lat, lon) => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+        setWeatherData(json);
+
+        const icon = json.weather[0].icon;
+        const iconData = `https://openweathermap.org/img/w/${icon}.png`;
+        setIconURL(iconData);
+        console.log("icon", iconData);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
   return (
     <WeatherBox>
-      <WeatherIcon>
-        <img src={Sun} alt="" />
-      </WeatherIcon>
-      <WeatherInfo>
-        <strong>Ansan</strong>
-        <p>
-          <span>9</span>° <span>/ </span>
-          <span>12</span>°
-        </p>
-      </WeatherInfo>
+      {weatherData && iconURL && (
+        <>
+          <WeatherIcon>
+            <img src={iconURL} alt="weather icon" />
+          </WeatherIcon>
+          <WeatherInfo>
+            <strong>{weatherData.name.split("-")[0].trim()}</strong>
+            <p>
+              <span>{Math.round(weatherData.main.temp_min)}</span>°{" "}
+              <span>/ </span>
+              <span>{Math.round(weatherData.main.temp_max)}</span>°
+            </p>
+          </WeatherInfo>
+        </>
+      )}
     </WeatherBox>
   );
 }
@@ -23,12 +67,12 @@ const WeatherBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 100px;
+  gap: 50px;
 `;
 
 const WeatherIcon = styled.div`
-  width: 300px;
-  height: 300px;
+  width: 400px;
+  height: 400px;
 
   img {
     width: 100%;
