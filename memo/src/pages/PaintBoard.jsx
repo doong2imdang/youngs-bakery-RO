f8abf6a701a87f8b40a321c87c2957bebb2d8763
KIm Seo Young <jmh7367@naver.com>
2024-03-13 17:01:58 +0900

    Design: 그림판 골격 완성

--- memo/src/pages/PaintBoard.jsx
+++ memo/src/pages/PaintBoard.jsx
@@ -1,9 +1,182 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function PaintBoard() {
   return (
-    <>
-      <div></div>
-    </>
+    <PaintBoardStyle>
+      <Canvas></Canvas>
+      <div className="btns-colors">
+        <ul className="btns">
+          <li>
+            <button className="draw-mode-btn" type="button">
+              Fill
+            </button>
+          </li>
+          <li>
+            <button className="erase-btn" type="button">
+              Erase
+            </button>
+          </li>
+          <li>
+            <button className="save-btn" type="button">
+              Save
+            </button>
+          </li>
+        </ul>
+        <ul className="colors">
+          <li>
+            <input type="color" id="color" />
+          </li>
+          <li>
+            <div
+              className="color-option"
+              style={{ backgroundColor: "#000000" }}
+              data-color="#000000"
+            ></div>
+          </li>
+          <li>
+            <div
+              className="color-option"
+              style={{ backgroundColor: "#ffffff" }}
+              data-color="#ffffff"
+            ></div>
+          </li>
+          <li>
+            <div
+              className="color-option"
+              style={{ backgroundColor: "#f40407" }}
+              data-color="#f40407"
+            ></div>
+          </li>
+          <li>
+            <div
+              className="color-option"
+              style={{ backgroundColor: "#fcfd07" }}
+              data-color="#fcfd07"
+            ></div>
+          </li>
+          <li>
+            <div
+              className="color-option"
+              style={{ backgroundColor: "#29fd09" }}
+              data-color="#29fd09"
+            ></div>
+          </li>
+          <li>
+            <div
+              className="color-option"
+              style={{ backgroundColor: "#32fefd" }}
+              data-color="#32fefd"
+            ></div>
+          </li>
+          <li>
+            <div
+              className="color-option"
+              style={{ backgroundColor: "#1d02fe" }}
+              data-color="#1d02fe"
+            ></div>
+          </li>
+          <li>
+            <div
+              className="color-option"
+              style={{ backgroundColor: "#fe00ff" }}
+              data-color="#fe00ff"
+            ></div>
+          </li>
+        </ul>
+        <input id="line-width" type="range" min="1" max="10" value="5" />
+      </div>
+    </PaintBoardStyle>
   );
 }
+
+const PaintBoardStyle = styled.div`
+  padding-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .btns-colors {
+    width: 500px;
+    display: grid;
+    grid-template-areas:
+      "btns range"
+      "colors range";
+    gap: 4px;
+    margin-top: 10px;
+  }
+
+  .btns {
+    grid-area: btns;
+    display: flex;
+    gap: 7.5px;
+  }
+
+  .draw-mode-btn,
+  .erase-btn,
+  .save-btn {
+    width: 70px;
+    height: 27px;
+    font-size: 18px;
+  }
+
+  .colors {
+    display: flex;
+    grid-area: colors;
+    gap: 2px;
+    justify-self: start;
+    align-items: center;
+    background: #e0e0e0;
+    padding-right: 2px;
+    height: 24px;
+    margin-top: 5px;
+  }
+
+  #color {
+    width: 19px;
+    height: 23px;
+    padding: 0;
+  }
+
+  .color-option {
+    width: 20px;
+    height: 20px;
+    box-shadow: inset 1.5px 1.5px 1px black;
+    cursor: pointer;
+  }
+
+  #line-width {
+    grid-area: range;
+    justify-self: end;
+    width: 208px;
+  }
+
+  input[type="range"] {
+    width: 100%;
+    appearance: none;
+    background: transparent;
+  }
+
+  input[type="range"]::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    background: #b0b7b8;
+    box-shadow: inset -1px -1px 1px black, inset 1px 1px 1px white;
+    margin-top: -14px;
+    cursor: pointer;
+    height: 25px;
+    width: 15px;
+  }
+
+  input[type="range"]::-webkit-slider-runnable-track {
+    width: 100%;
+    height: 1px;
+    cursor: pointer;
+    box-shadow: -1px -1px 1px #000000, inset 1px 1px 1px white;
+  }
+`;
+
+const Canvas = styled.canvas`
+  width: 500px;
+  height: 500px;
+  background: #ffffff;
+`;
